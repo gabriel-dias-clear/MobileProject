ee01cf7927b53ee800ee99d17f4608885452f83d
bielFernandes <gabriel.dias.clear@gmail.com>
2023-11-06 11:47:33 -0300

    app biscuit

--- App.js
+++ App.js
@@ -1,43 +1,70 @@
-import {Component} from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import React, {Component, useState, useEffect} from 'react';
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
-class App extends Component {
-  render(){
-    let name = 'Estevão Trabalhos'
+export default function App(){
+
+  const [pathImage, setPathImage] = useState(require('./img/biscoito.png'))
+  const array = ['Dia Bonito', 'Opa que legal', 'Caramba que top', 'Genial em', 'Ulalau']
+  const [frase, setFrase] = useState('Clique e sorteie uma frase')
+  
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  function HandleButton(){
+    
+      let aletoricNumber = getRandomInt(0, array.length)
+      setPathImage(require('./img/biscoitoAberto.png'))
+      setFrase(array[aletoricNumber])
+    
+  }
+
     return(
       <View style={styles.container}>
-        <Text style={{ color: 'red', fontSize:30}}>
-          Sujeito Programador
-        </Text>
-          <Jobs largura={400} altura={400}/> 
-        <Text style={{width:'100', textAlign:'center', fontSize:50, color: 'red'}}>
-          {name}
-        </Text>
+        <Image
+          source={pathImage}
+          style={styles.imageBisc}
+        />
+
+        <Text style={styles.text}>{frase}</Text>
+        <TouchableOpacity onPress={HandleButton} style={styles.btn}>
+          <View>
+            <Text style={styles.textBtn}>
+              Clique aqui!
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
-
     )
-    }
-  }
 
-export default App;
-
-
-class Jobs extends Component{
-    render(){
-      return(
-          <Image
-            source={{ uri:'https://sujeitoprogramador.com/steve.png' }}
-            style={{width:this.props.largura, height:this.props.altura}}
-          />
-      );
-    }
-  }
+}
 
   const styles = StyleSheet.create({
     container: {
       flex:1,
-      backgroundColor: "#CAD8E1",
+      backgroundColor: "#9E6606",
       alignItems: 'center',
       justifyContent: 'center'
     },
+    imageBisc:{
+      width:330,
+      height:280,
+      marginBottom:25
+    },
+    text:{
+      fontSize:25,
+      color:'white',
+      fontStyle: 'italic'
+    },
+    btn:{
+      backgroundColor:'green',
+      padding:10,
+      marginTop:8
+    },
+    textBtn:{
+      color:'white'
+    }
   });
+
